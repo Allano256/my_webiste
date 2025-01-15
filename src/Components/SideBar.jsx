5a40189414db6ce6f5a126bd415892298e0704c8
allan zizinga <allanzizinga@yahoo.com>
2025-01-16 00:56:50 +0100

    Changed background of navigation and image path

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -3,7 +3,7 @@ import styles from './SideBar.module.css'
 function SideBar() {
     return(
    <div className={styles.SideBar}>
-     <img src="/public/me.jpg" alt="an image of man standing" />
+     <img src="/me.jpg" alt="an image of man standing" />
    </div>
     )
     
