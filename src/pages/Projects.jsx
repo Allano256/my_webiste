b136ea4de3078919cd0ee36a7b57ab804a1a5c78
allan zizinga <allanzizinga@yahoo.com>
2025-01-21 00:31:43 +0100

    Created the projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,29 @@
+import { NavLink } from "react-router-dom"
 import PageNavigation from "./PageNavigation"
+import styles from './Projects.module.css'
 
 function Projects() {
     return (
         <>
          <PageNavigation />
-        <div>
-            <h3>Projects coming soon.</h3>
-        </div></>
+       <div className={styles.project}>
+        <h1> Projects page</h1>
+        <p>Follow the links in the description <br /> to access the live versions.</p>
+        <div className={styles.item1}>
+            <h3>The restaurant booking application.</h3>
+            <img src="/public/restaurant_img.png" alt="an image of the restaurant layout" />
+            <NavLink to='https://allano256.github.io/restaurant_app/' ><h3>Live Version.</h3> </NavLink>
+        </div>
+        <div className={styles.item2}>
+        <h3>A Football Agency Website.</h3>
+            <img src="/public/top_talent.png" alt="an image of the restaurant layout" />
+            <NavLink to=' https://allano256.github.io/top-talent/' ><h3>Live Version.</h3> </NavLink>
+        </div>
+        {/* <div className="item"></div>
+        <div className="item2"></div> */}
+       </div>
+        
+        </>
        
     )
 }
